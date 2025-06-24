917ede8c2bca2535a2a303268049aff2ff4a8569
VUPPU KULADEEP <vuppukuladeep@gmail.com>
2025-06-24 11:51:54 +0530

    added style sheet

--- script.js
+++ script.js
@@ -8,6 +8,15 @@ button.addEventListener('click' ,() =>{
     if (text == ''){
         return
     }
+    const done = document.createElement('button')
+    done.textContent = '✅'
+    done.addEventListener('click', () =>{
+        const over = document.createElement('p')
+        over.textContent = 'done'
+        li.removeChild(delbtn)
+        li.removeChild(done)
+        li.appendChild(t)
+    });
     const delbtn = document.createElement('button')
     delbtn.textContent = '❌'
     delbtn.addEventListener('click', () =>{
@@ -15,6 +24,7 @@ button.addEventListener('click' ,() =>{
     });
     const li = document.createElement('li')
     li.textContent = text
+    li.appendChild(done)
     li.appendChild(delbtn)
     list.appendChild(li)
     input.value = '';
